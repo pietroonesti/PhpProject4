1e1e9af85ba4bcf5d0346a310ceada2da46df260
pietroonesti <pietro.onesti@gmail.com>
2023-08-26 11:47:52 +0200

    primo progetto con git da NETBEANS

---
+++ page6.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Progetto creato da Pietro Onesti 
+ * data creazione: 26 ago 2023
+ * Test utilizzo github in Netbeans18
+ * funzione del file: test stage    
+ */
+
+echo 'pagina6!!';
